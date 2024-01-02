188d371d8514ecad61daa8296e4ea3d3da2d886e
Yash108108 <153550187+Yash108108@users.noreply.github.com>
2024-01-02 07:10:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/Yash108108/VJ-FILTER-BOT /VJ-FILTER-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
